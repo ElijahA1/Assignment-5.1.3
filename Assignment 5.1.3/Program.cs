ff9a1f8984bfdf2075268d7c25d45d61cf8b4aa9
ElijahA1 <elijah.abourezk@outlook.com>
2025-08-04 12:56:06 -0700

    Add project files.

---
+++ Assignment 5.1.3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment_5._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4 };
+
+            Console.WriteLine(containsRepeatValues(array));
+        }
+
+        static bool containsRepeatValues(int[] array) 
+        {
+            HashSet<int> set = new();
+
+            foreach (int i in array) 
+            {
+                if (!set.Add(i)) 
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
